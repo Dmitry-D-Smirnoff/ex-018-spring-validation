ab8aaac27cde83000cfeeaf552ff6de09bb16097
Dmitry Smirnoff <d.smirnov@alfacapital.ru>
2021-11-23 11:05:24 +0300

    +

---
+++ src/main/resources/sql/2.init-data.sql
@@ -0,0 +1,12 @@
+INSERT INTO EXAMPLE018.t_country(country_name) VALUES ('Austria');
+INSERT INTO EXAMPLE018.t_country(country_name) VALUES ('Greece');
+INSERT INTO EXAMPLE018.t_country(country_name) VALUES ('France');
+
+INSERT INTO EXAMPLE018.t_city(city_name, country_id) VALUES ('Vienna', 1);
+INSERT INTO EXAMPLE018.t_city(city_name, country_id) VALUES ('Athens', 2);
+INSERT INTO EXAMPLE018.t_city(city_name, country_id) VALUES ('Paris', 3);
+INSERT INTO EXAMPLE018.t_city(city_name, country_id) VALUES ('Marseille', 3);
+
+INSERT INTO EXAMPLE018.t_role(role_name) VALUES ('ROLE_ADMIN');
+INSERT INTO EXAMPLE018.t_role(role_name) VALUES ('ROLE_USER');
+INSERT INTO EXAMPLE018.t_role(role_name) VALUES ('ROLE_UNUSED');
