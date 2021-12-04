f3ccc96e29b36fd420d898dded37477e328fab08
Dmitry Smirnoff <d.smirnov@alfacapital.ru>
2021-12-04 18:19:50 +0300

    Вызов Java-валидации Entity в начале метода кастомной валидации Entity

--- src/main/java/com/example/sdd/entity/validation/CountryEntityValidator.java
+++ src/main/java/com/example/sdd/entity/validation/CountryEntityValidator.java
@@ -11,9 +11,10 @@ import com.example.sdd.validation.ErrorDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
-import java.util.ArrayList;
+import javax.validation.ValidatorFactory;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 import static com.example.sdd.validation.ErrorCode.ERROR_CODE_PERSON_NAME_DUPLICATION;
 import static com.example.sdd.validation.ValidationErrorMessages.VALID_COUNTRY_MUST_HAVE_UNIQUE_COUNTRY_NAME;
@@ -22,17 +23,19 @@ import static com.example.sdd.validation.ValidationErrorMessages.VALID_COUNTRY_M
 public class CountryEntityValidator implements EntityValidator<CountryEntity> {
 
     private final CountryService countryService;
-
+    private final ValidatorFactory validatorFactory;
     private final PersonEntityValidator personEntityValidator;
 
-    public CountryEntityValidator(CountryService countryService, PersonEntityValidator personEntityValidator) {
+    public CountryEntityValidator(CountryService countryService, ValidatorFactory validatorFactory, PersonEntityValidator personEntityValidator) {
         this.countryService = countryService;
+        this.validatorFactory = validatorFactory;
         this.personEntityValidator = personEntityValidator;
     }
 
     @Override
     public List<ErrorDetails> collectValidationErrors(CountryEntity target, Class<?> hint, Class<CountryEntity> targetType) {
-        List<ErrorDetails> errors = new ArrayList<>();
+        List<ErrorDetails> errors = validatorFactory.getValidator().validate(target)
+                .stream().map(ErrorDetails::new).collect(Collectors.toList());
 
         for (PersonEntity personEntity : target.getPersons()) {
             errors.addAll(personEntityValidator.collectValidationErrors(
--- src/main/java/com/example/sdd/entity/validation/PersonEntityValidator.java
+++ src/main/java/com/example/sdd/entity/validation/PersonEntityValidator.java
@@ -7,9 +7,10 @@ import com.example.sdd.validation.ErrorDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
-import java.util.ArrayList;
+import javax.validation.ValidatorFactory;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 import static com.example.sdd.validation.ErrorCode.ERROR_CODE_PERSON_NAME_DUPLICATION;
 import static com.example.sdd.validation.ValidationErrorMessages.VALID_PERSON_MUST_HAVE_UNIQUE_PERSON_NAME;
@@ -18,14 +19,17 @@ import static com.example.sdd.validation.ValidationErrorMessages.VALID_PERSON_MU
 public class PersonEntityValidator implements EntityValidator<PersonEntity> {
 
     private final PersonService personService;
+    private final ValidatorFactory validatorFactory;
 
-    public PersonEntityValidator(PersonService personService) {
+    public PersonEntityValidator(PersonService personService, ValidatorFactory validatorFactory) {
         this.personService = personService;
+        this.validatorFactory = validatorFactory;
     }
 
     @Override
     public List<ErrorDetails> collectValidationErrors(PersonEntity target, Class<?> hint, Class<PersonEntity> targetType) {
-        List<ErrorDetails> errors = new ArrayList<>();
+        List<ErrorDetails> errors = validatorFactory.getValidator().validate(target)
+                .stream().map(ErrorDetails::new).collect(Collectors.toList());
 
         List<PersonEntity> duplicates = personService.getPersonByName(target.getPersonName());
         if (!CollectionUtils.isEmpty(duplicates) && (PersonCreate.class.equals(hint)
